370a894ada41b866838f33389a75ee1501a9cc46
DomVictor <victor.m7070@gmail.com>
2017-06-24 18:18:56 -0300

    PizzaMania0.0.0.09

--- src/view/FormProduto.java
+++ src/view/FormProduto.java
@@ -37,7 +37,6 @@ public class FormProduto extends JFrame {
 		produto = new Produto();
 		produto = produto1;
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, ScreenSize.getScreenWidth(), ScreenSize.getScreenHeight()-30);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
